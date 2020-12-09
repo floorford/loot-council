c8461d3051cdc2a0adc9d6f0316bc3f72ed74dd8
Flo Orford <flo.orford@hotmail.com>
2020-12-09 15:11:26 +0000

    deleted second client codebae and just put my react app inside of this project, committed code is all of the setup and me removing some unneeded testing giles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+// window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+import LootCouncil from "./components/LootCouncil";
+
+ReactDOM.render(<LootCouncil />, document.getElementById("root"));
---
+++ resources/js/components/LootCouncil.tsx
@@ -0,0 +1,8 @@
+// window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+import React from "react";
+
+const LootCouncil = (): JSX.Element => {
+    return <div>hi</div>;
+};
+
+export default LootCouncil;
